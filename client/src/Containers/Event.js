99c33ee85b753c05771b60d5e85e3003a16181ba
Garret <gwiats@gmail.com>
2021-08-04 16:32:46 -0400

    starting first time login prompts

--- client/src/Containers/Event.js
+++ client/src/Containers/Event.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const Event = () => {
   const [event, setEvent] = useState({})
+  const [eventStatus, setEventStatus] = useState(true)
 
   useEffect(() => {
     fetch('/events')
@@ -9,7 +10,9 @@ const Event = () => {
       if (res.ok) {
         res.json()
         .then(data => {
-          if (data.errors) {
+          if (data === null) {
+            setEventStatus(false)
+          } else if (data.errors) {
             alert(data.errors)
           } else {
             console.log(data)
@@ -20,11 +23,29 @@ const Event = () => {
     })
   }, [])
 
-  return (
-    <div>
+  if (eventStatus === true) {
+    return (
+      <div>
 
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h3>Thank you for choosing EasyWed to plan your big day. To get started please enter some initial information</h3>
+        <form>
+          <label>Name the Occasion </label>
+          <input></input>
+          <br/>
+          <label>What budget are you working with? </label>
+          <input></input>
+          <br/>
+          <label>How many people does your venue hold? </label>
+          <input></input>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Event
