e80dbffe80cc8528891c2ca7b45bd643cd738403
Garret <gwiats@gmail.com>
2021-08-05 01:11:29 -0400

    building reception model

---
+++ app/serializers/reception_serializer.rb
@@ -0,0 +1,3 @@
+class ReceptionSerializer < ActiveModel::Serializer
+  attributes :id
+end
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -6,16 +6,15 @@ const NavBar = ({loggedIn, logout}) => {
 
   if (loggedIn === false) {
     return (
-      <div>
-        {/* <img className="Resize" alt="Globe" src='https://cdn.shopify.com/s/files/1/0010/4225/1812/products/Enamel_Globe-Trotter_01_Top-View.png?v=1603458946'/> */}
+      <div>  
         <h2>Welcome to EasyWed where we make wedding planning easy.</h2>
       </div>
     )
   } else {
     return (
       <div>
-        {/* <img className="Resize"src='https://cdn.shopify.com/s/files/1/0010/4225/1812/products/Enamel_Globe-Trotter_01_Top-View.png?v=1603458946'/> */}
         <NavLink className='App-link' to='/'>Home</NavLink>
+        <NavLink className='App0link' to='/receptions'>Reception</NavLink>
         <button onClick={logout}>Log Out</button>
       </div>
     )
