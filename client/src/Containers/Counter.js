d6214e13a6931628c2df3349ead7cf7c0d339386
Garret <gwiats@gmail.com>
2022-06-12 00:13:31 -0400

    moved counter to its own file in preparation to add other things

---
+++ client/src/Containers/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0)
+
+  const handleIncrease = () => {// increases counter by 1
+    setCounter(counter + 1)
+  }
+
+  const handleDecrease = () => {// decreases counter by 1
+    setCounter(counter - 1)
+  }
+  
+  return (
+    <div>
+      <p>This is a random counter, you can increase or decrease it's count using the buttons below {counter}</p>
+      <button onClick={handleIncrease}>Plus 1</button> <button onClick={handleDecrease}>Minus 1</button>
+    </div>
+  )
+}
+
+export default Counter
--- client/src/Containers/Oddball.js
+++ client/src/Containers/Oddball.js
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import Counter from './Counter'
 
 const Oddball = () => {
-  const [counter, setCounter] = useState(0)
-
-  const handleIncrease = () => {// increases counter by 1
-    setCounter(counter + 1)
-  }
-
-  const handleDecrease = () => {// decreases counter by 1
-    setCounter(counter - 1)
-  }
 
   return (
     <div>
-      <p>This is a random counter, you can increase or decrease it's count using the buttons below {counter}</p>
-      <button onClick={handleIncrease}>Plus 1</button> <button onClick={handleDecrease}>Minus 1</button>
+     {Counter()}
+     <div className={'GameBox'}></div>
     </div>
   )
 }
